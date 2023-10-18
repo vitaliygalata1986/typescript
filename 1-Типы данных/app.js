a511e8c2ab8de4e6b4f06b8234363239c47b923d
Vitaliy Galata <nertis44@gmail.com>
2023-10-18 14:18:37 +0300

    typing object

--- 1-Типы данных/app.js
+++ 1-Типы данных/app.js
@@ -1,7 +1,12 @@
 "use strict";
-let revenue = 1000;
-let bonus = 500;
-let c = 'sdf';
-let d = true;
-let res = revenue + bonus;
-console.log(res);
+// типизация объекта
+function getInfoPeople(userEntity) {
+    return `${userEntity.firstname} ${userEntity.lastname}`;
+}
+const user = {
+    firstname: 'Vitaliy',
+    lastname: 'Galata',
+    city: 'Nikolaev',
+    age: 37,
+};
+console.log(getInfoPeople(user));
--- 1-Типы данных/app.ts
+++ 1-Типы данных/app.ts
@@ -1,7 +1,50 @@
-let revenue: number = 1000;
-let bonus: number = 500;
-let c: string = 'sdf';
-let d: boolean = true;
+// типизация объекта
+function getInfoPeople(userEntity: {
+  firstname: string;
+  lastname: string;
+}): string {
+  return `${userEntity.firstname} ${userEntity.lastname}`;
+}
 
-let res: number = revenue + bonus;
-console.log(res); // 1500
+const user = {
+  firstname: 'Vitaliy',
+  lastname: 'Galata',
+  city: 'Nikolaev',
+  age: 37,
+  skills: {
+    dev: true,
+    devops: true,
+  },
+};
+
+console.log(getInfoPeople(user)); // Vitaliy Galata
+
+/*
+Упражнение.
+Нам приходит json:
+{
+	"officeId": 45,
+	"isOpened": false,
+	"contacts": {
+		"phone": "+79100000000",
+		"email": "my@email.ru",
+		"address": {
+			"city": "Москва"
+		}
+	}
+}
+*/
+
+// Нужно его типизировать.
+
+let userInfo: {
+  officeId: number;
+  isOpened: boolean;
+  contacts: {
+    phone: string;
+    email: string;
+    address: {
+      city: string;
+    };
+  };
+};
