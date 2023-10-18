5b49fbb19dc338937f6c7d2d19f2c754aea7fe2a
Vitaliy Galata <nertis44@gmail.com>
2023-10-18 14:56:56 +0300

    typles

--- 1-Типы данных/app.js
+++ 1-Типы данных/app.js
@@ -1,7 +1,13 @@
 "use strict";
-let revenue = 1000;
-let bonus = 500;
-let c = 'sdf';
-let d = true;
-let res = revenue + bonus;
-console.log(res);
+// Проблема модификаций кортежев и массивов.
+// переопределим наш нулевой элемент кортежа двойкой:
+const newSkillsNew = [1, 'Dev'];
+newSkillsNew[0] = 2;
+console.log(newSkillsNew); // [ 2, 'Dev' ]
+// Воспользуемся модификатором в TS readonly:
+const newSkillsNewAll = [1, 'Dev'];
+// newSkillsNewAll[0] = 2; // Данный кортеж не модифицированный.
+// console.log(newSkillsNewAll);
+// альтернатива : string[] - использование Generic
+const skillsGeneric = ['Dev', 'DevOps']; // Array<string> - это всего лишь массив строк
+console.log(skillsGeneric); // [ 'Dev', 'DevOps' ]
--- 1-Типы данных/app.ts
+++ 1-Типы данных/app.ts
@@ -1,7 +1,22 @@
-let revenue: number = 1000;
-let bonus: number = 500;
-let c: string = 'sdf';
-let d: boolean = true;
+// Проблема модификаций кортежев и массивов.
 
-let res: number = revenue + bonus;
-console.log(res); // 1500
+// переопределим наш нулевой элемент кортежа двойкой:
+const newSkillsNew: [number, string] = [1, 'Dev'];
+newSkillsNew[0] = 2;
+console.log(newSkillsNew); // [ 2, 'Dev' ]
+
+// Воспользуемся модификатором в TS readonly:
+const newSkillsNewAll: readonly [number, string] = [1, 'Dev'];
+// newSkillsNewAll[0] = 2; // Данный кортеж не модифицированный.
+// console.log(newSkillsNewAll);
+
+// альтернатива : string[] - использование Generic
+const skillsGeneric: Array<string> = ['Dev', 'DevOps']; // Array<string> - это всего лишь массив строк
+console.log(skillsGeneric); // [ 'Dev', 'DevOps' ]
+
+// также для readonly есть специальный тип ReadonlyArray
+const skillsReadonlyArray: ReadonlyArray<string> = ['Dev', 'DevOps'];
+// skillsReadonlyArray[0] = 'Testing';
+// Пушить тоже не можем:
+// skillsReadonlyArray.push('test');
+// В JS мы не можем зафиксировать длину, модификацию массива.
