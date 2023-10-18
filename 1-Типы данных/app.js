00761d1362b3dedbcdeb45b5775db3c0a2c674ea
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-10-18 11:46:46 +0300

    initial commit

---
+++ 1-Типы данных/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let revenue = 1000;
+let bonus = 500;
+let c = 'sdf';
+let d = true;
+let res = revenue + bonus;
+console.log(res);
---
+++ 1-Типы данных/app.ts
@@ -0,0 +1,7 @@
+let revenue: number = 1000;
+let bonus: number = 500;
+let c: string = 'sdf';
+let d: boolean = true;
+
+let res: number = revenue + bonus;
+console.log(res); // 1500
