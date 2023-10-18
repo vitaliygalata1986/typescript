96e2fb6fc7d3030c9f6961bd3a99920126bd5222
Vitaliy Galata <nertis44@gmail.com>
2023-10-18 15:58:32 +0300

    Пример типизации функции, котторая отсылает запрос на сервер и возращает ответ

--- 1-Типы данных/app.ts
+++ 1-Типы данных/app.ts
@@ -1,7 +1,62 @@
-let revenue: number = 1000;
-let bonus: number = 500;
-let c: string = 'sdf';
-let d: boolean = true;
+/* Запрос */
+/*
+{
+	"topicId": 5,
+	"status": "published" // "draft", "deleted"
+}
+*/
 
-let res: number = revenue + bonus;
-console.log(res); // 1500
+/* Ответ */
+/*
+[
+	{
+		"question": "Как осуществляется доставка?",
+		"answer": "быстро!",
+		"tags": [
+			"popular",
+			"new"
+		],
+		"likes": 3,
+		"status": "published"
+	}
+]
+*/
+
+// задача - типизировать функцию
+
+/*
+async function getFaqs(req) {
+	const res = await fetch('/faqs', {
+		method: 'POST',
+		body: JSON.stringify(req)
+	});
+	const data = await res.json();
+	return data;
+}
+*/
+
+enum QuestionStatus { // сделаем через enum, так как имеем ограниченное число ответов
+  Published = 'published',
+  Draft = 'draft',
+  Deleted = 'deleted',
+}
+async function getFaqs(req: {
+  topicId: number;
+  status: QuestionStatus;
+}): Promise<
+  {
+    // здесь пишем, что функция возвращает - возвращает массив объектов - в конце пишем []
+    question: string;
+    answer: string;
+    tags: string[]; // массив стрингов
+    likes: number;
+    status: QuestionStatus;
+  }[]
+> {
+  const res = await fetch('/faqs', {
+    method: 'POST',
+    body: JSON.stringify(req),
+  });
+  const data = await res.json();
+  return data;
+}
