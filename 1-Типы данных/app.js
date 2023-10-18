c11c4e5a3c3d273c0445709c9bf8db486e710ed1
Vitaliy Galata <nertis44@gmail.com>
2023-10-18 14:33:58 +0300

    typing array

--- 1-Типы данных/app.js
+++ 1-Типы данных/app.js
@@ -1,7 +1,18 @@
 "use strict";
-let revenue = 1000;
-let bonus = 500;
-let c = 'sdf';
-let d = true;
-let res = revenue + bonus;
-console.log(res);
+// типизируем массив - указывем явно - массив строк
+const skills = ['Dev', 'DevOps', 'Testing'];
+// типизируем не однородный массив
+const skillsAll = ['Dev', 'DevOps', 1];
+// any не стоит использовать.
+for (const skill of skills) {
+    console.log(skill.toLowerCase());
+}
+// Явно укажем входящий параметр для метода filter():
+const filterArray = skills.filter((s) => s !== 'DevOps');
+console.log(filterArray); // [ 'Dev', 'Testing' ]
+// В итоге во всех фильтрах мы правильно использовали поведение типов:
+const filterArrayMapReduce = skills
+    .filter((s) => s !== 'DevOps')
+    .map((s) => s + '! ')
+    .reduce((a, b) => a + b);
+console.log(filterArrayMapReduce); // Dev! Testing!
--- 1-Типы данных/app.ts
+++ 1-Типы данных/app.ts
@@ -1,7 +1,23 @@
-let revenue: number = 1000;
-let bonus: number = 500;
-let c: string = 'sdf';
-let d: boolean = true;
+// типизируем массив - указывем явно - массив строк
+const skills: string[] = ['Dev', 'DevOps', 'Testing'];
 
-let res: number = revenue + bonus;
-console.log(res); // 1500
+// типизируем не однородный массив
+const skillsAll: any[] = ['Dev', 'DevOps', 1];
+
+// any не стоит использовать.
+
+for (const skill of skills) {
+  console.log(skill.toLowerCase());
+}
+
+// Явно укажем входящий параметр для метода filter():
+const filterArray = skills.filter((s: string) => s !== 'DevOps');
+console.log(filterArray); // [ 'Dev', 'Testing' ]
+
+// В итоге во всех фильтрах мы правильно использовали поведение типов:
+const filterArrayMapReduce = skills
+  .filter((s: string) => s !== 'DevOps')
+  .map((s) => s + '! ')
+  .reduce((a, b) => a + b);
+
+console.log(filterArrayMapReduce); // Dev! Testing!
