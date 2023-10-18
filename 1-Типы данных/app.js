a70c3a6cb2742011b76618dd17b575eebc32544a
Vitaliy Galata <nertis44@gmail.com>
2023-10-18 15:43:47 +0300

    enums

--- 1-Типы данных/app.js
+++ 1-Типы данных/app.js
@@ -1,7 +1,88 @@
 "use strict";
-let revenue = 1000;
-let bonus = 500;
-let c = 'sdf';
-let d = true;
-let res = revenue + bonus;
-console.log(res);
+/*
+const res = {
+  message: 'Платеж успешный',
+  statusCode: 1,
+};
+*/
+// 1 - успех
+// 2 - в процессе
+// 3 - отклонен
+// Если мы будем модифицировать объект, то этому статусу можем присвоить другое значение.
+// Или мы можем:
+/*
+if (res.statusCode === 'succes') {
+  // спец. сделали опечатку
+}
+*/
+// создадим наборы констант для каждого статуса.
+var StatusCode;
+(function (StatusCode) {
+    StatusCode[StatusCode["SUCCESS"] = 0] = "SUCCESS";
+    StatusCode[StatusCode["IN_PROCESS"] = 1] = "IN_PROCESS";
+    StatusCode[StatusCode["FAILED"] = 2] = "FAILED";
+})(StatusCode || (StatusCode = {}));
+const res = {
+    message: 'Платеж успешный',
+    statusCode: StatusCode.SUCCESS, // типизируем statusCode
+};
+if (res.statusCode === StatusCode.SUCCESS) {
+    // в итоге мы не можем опечататься, получить другие значения
+}
+// Enum может быть и строковым
+var StatusCodeString;
+(function (StatusCodeString) {
+    StatusCodeString["SUCCESS"] = "s";
+    StatusCodeString["IN_PROCESS"] = "p";
+    StatusCodeString["FAILED"] = "f";
+})(StatusCodeString || (StatusCodeString = {}));
+// Гетерогенные enum
+var StatusCodeGeter;
+(function (StatusCodeGeter) {
+    StatusCodeGeter[StatusCodeGeter["SUCCESS"] = 10] = "SUCCESS";
+    StatusCodeGeter["IN_PROCESS"] = "p";
+    StatusCodeGeter["FAILED"] = "f";
+})(StatusCodeGeter || (StatusCodeGeter = {}));
+// хотим принимать енамы этого типа: StatusCodeGeter
+function action(status) {
+    console.log('success');
+    console.log(status);
+}
+action(StatusCodeGeter.SUCCESS); // saccess 10
+action(10); // тоже будет валидное значение, так как SUCCESS = 10 присутствует в StatusCodeGeter
+// вот так будет проблема, там как енам гетерогенный, но продолжает быть числовым
+// action('p');
+action(StatusCodeGeter.IN_PROCESS); // лучше вот так
+var Roles;
+(function (Roles) {
+    Roles[Roles["ADMIN"] = 0] = "ADMIN";
+    Roles[Roles["USER"] = 1] = "USER";
+})(Roles || (Roles = {}));
+// Enum могут быть рассчетными:
+var RolesAll;
+(function (RolesAll) {
+    RolesAll[RolesAll["ADMIN"] = 1] = "ADMIN";
+    RolesAll[RolesAll["USER"] = 2] = "USER";
+})(RolesAll || (RolesAll = {}));
+// Даже функцию можно передать:
+function compute() {
+    return 3;
+}
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 1] = "ADMIN";
+    Role[Role["USER"] = compute()] = "USER";
+})(Role || (Role = {}));
+// Енамы похожи на объекты.
+var Roleses;
+(function (Roleses) {
+    Roleses[Roleses["ADMIN"] = 1] = "ADMIN";
+    Roleses[Roleses["USER"] = compute()] = "USER";
+})(Roleses || (Roleses = {}));
+function test(x) {
+    return x;
+}
+console.log(test(Roleses)); // { '1': 'ADMIN', '3': 'USER', ADMIN: 1, USER: 3 }
+// Так как мы объявляем роли как константы, то компилятор ищет все места, где упоминается Roleses и заменяет его на константы.
+// Так как таких мес нет, то ничего и не будет.
+const res2 = 1 /* RolesesConst.ADMIN */;
--- 1-Типы данных/app.ts
+++ 1-Типы данных/app.ts
@@ -1,7 +1,108 @@
-let revenue: number = 1000;
-let bonus: number = 500;
-let c: string = 'sdf';
-let d: boolean = true;
+/*
+const res = {
+  message: 'Платеж успешный',
+  statusCode: 1,
+};
+*/
 
-let res: number = revenue + bonus;
-console.log(res); // 1500
+// 1 - успех
+// 2 - в процессе
+// 3 - отклонен
+
+// Если мы будем модифицировать объект, то этому статусу можем присвоить другое значение.
+// Или мы можем:
+/*
+if (res.statusCode === 'succes') {
+  // спец. сделали опечатку
+}
+*/
+
+// создадим наборы констант для каждого статуса.
+enum StatusCode {
+  SUCCESS,
+  IN_PROCESS,
+  FAILED,
+}
+const res = {
+  message: 'Платеж успешный',
+  statusCode: StatusCode.SUCCESS, // типизируем statusCode
+};
+
+if (res.statusCode === StatusCode.SUCCESS) {
+  // в итоге мы не можем опечататься, получить другие значения
+}
+
+// Enum может быть и строковым
+
+enum StatusCodeString {
+  SUCCESS = 's',
+  IN_PROCESS = 'p',
+  FAILED = 'f',
+}
+
+// Гетерогенные enum
+
+enum StatusCodeGeter {
+  SUCCESS = 10,
+  IN_PROCESS = 'p',
+  FAILED = 'f',
+}
+
+// хотим принимать енамы этого типа: StatusCodeGeter
+function action(status: StatusCodeGeter) {
+  console.log('success');
+  console.log(status);
+}
+action(StatusCodeGeter.SUCCESS); // saccess 10
+action(10); // тоже будет валидное значение, так как SUCCESS = 10 присутствует в StatusCodeGeter
+
+// вот так будет проблема, там как енам гетерогенный, но продолжает быть числовым
+// action('p');
+action(StatusCodeGeter.IN_PROCESS); // лучше вот так
+
+enum Roles {
+  ADMIN,
+  USER,
+}
+
+// Enum могут быть рассчетными:
+enum RolesAll {
+  ADMIN = 1,
+  USER = ADMIN * 2,
+}
+
+// Даже функцию можно передать:
+function compute() {
+  return 3;
+}
+
+enum Role {
+  ADMIN = 1,
+  USER = compute(),
+}
+
+// Енамы похожи на объекты.
+enum Roleses {
+  ADMIN = 1,
+  USER = compute(),
+}
+
+function test(x: { ADMIN: number }) {
+  return x;
+}
+
+console.log(test(Roleses)); // { '1': 'ADMIN', '3': 'USER', ADMIN: 1, USER: 3 }
+
+// Константные енамы
+const enum RolesesConst {
+  ADMIN = 1,
+  USER = 2,
+}
+
+// Так как мы объявляем роли как константы, то компилятор ищет все места, где упоминается Roleses и заменяет его на константы.
+// Так как таких мес нет, то ничего и не будет.
+
+const res2 = RolesesConst.ADMIN;
+
+// То после компиляции:
+// const res2 = 1 /* RolesesConst.ADMIN */;
