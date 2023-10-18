6e799dfe232ca69f645684ee2ec0e9aee20df2b1
Vitaliy Galata <nertis44@gmail.com>
2023-10-18 14:51:14 +0300

    typles

--- 1-Типы данных/app.js
+++ 1-Типы данных/app.js
@@ -1,7 +1,16 @@
 "use strict";
-let revenue = 1000;
-let bonus = 500;
-let c = 'sdf';
-let d = true;
-let res = revenue + bonus;
-console.log(res);
+// Есть проблема, пытаемся в масив добавить число.
+// const a: string[] = ['str1', 'str2', 1];
+const a1 = ['str1', 1]; // typel это массив ограниченой длины, где каждый элемент типизированный
+const id = a1[0];
+const skillName = a1[1];
+// При этом мы не можем обращаться к элементу в не описания:
+// const q = a1[2];
+// Но мы можем пушить:
+a1.push('new');
+// Но даже после этого мы не можем обратиться ко второму элементу массива.
+// const q = a1[2];
+// Также в typles работают теже деструктуризации, что и в массивах.
+const ski = [1, 'Dev'];
+const [idSkill, nameSkill] = ski;
+console.log(idSkill, nameSkill); // 1 Dev
--- 1-Типы данных/app.ts
+++ 1-Типы данных/app.ts
@@ -1,7 +1,20 @@
-let revenue: number = 1000;
-let bonus: number = 500;
-let c: string = 'sdf';
-let d: boolean = true;
+// Есть проблема, пытаемся в масив добавить число.
+// const a: string[] = ['str1', 'str2', 1];
 
-let res: number = revenue + bonus;
-console.log(res); // 1500
+const a1: [string, number] = ['str1', 1]; // typel это массив ограниченой длины, где каждый элемент типизированный
+const id = a1[0];
+const skillName = a1[1];
+
+// При этом мы не можем обращаться к элементу в не описания:
+// const q = a1[2];
+
+// Но мы можем пушить:
+a1.push('new');
+
+// Но даже после этого мы не можем обратиться ко второму элементу массива.
+// const q = a1[2];
+
+// Также в typles работают теже деструктуризации, что и в массивах.
+const ski: [number, string] = [1, 'Dev'];
+const [idSkill, nameSkill] = ski;
+console.log(idSkill, nameSkill); // 1 Dev
