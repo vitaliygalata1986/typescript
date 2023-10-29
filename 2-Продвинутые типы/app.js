530d3b1c686ee24bb193d67587ac4d1970c9b988
Vitaliy Galata <nertis44@gmail.com>
2023-10-29 23:29:41 +0200

    use type aliases

--- 2-Продвинутые типы/app.js
+++ 2-Продвинутые типы/app.js
@@ -1,57 +1,22 @@
 "use strict";
-const arr = ['sdf', 1];
-// функция, которая будет логировать переданный нами id
-// принимает Union тип
-function logId(id) {
-    if (typeof id === 'string') {
-        console.log(id);
-    }
-    else if (typeof id === 'number') {
-        console.log(id);
-    }
-    else {
-        console.log(id);
-    }
+function fetchWithAuthAll(url, method) {
+    return 1;
 }
-// теперь можем функцию вызвать либо с number либо с string
-logId(1);
-logId('text');
-logId(true);
-// работа со cложными типами
-// функция будет принимать либо одну ошибку либо массив ошибок
-function logError(err) {
-    if (Array.isArray(err)) {
-        // если это массив
-        console.log(err);
-    }
-    else {
-        console.log(err);
-    }
-}
-console.log(logError(['error1', 'error2'])); // [ 'error1', 'error2' ]
-// принимаем объект, у которого есть либо свойство a/b
-function logObject(obj) {
-    // оператор in - проверяет, а есль ли тот или иной ключ в объекте
-    if ('a' in obj) {
-        console.log(obj.a);
-    }
-    else {
-        console.log(obj.b);
-    }
-}
-logObject({ a: 10, b: 1 }); // 10
-// функция, которая принимает multiply dist
-// в обоих случаях есть тип string
-function logMultipleIds(a, b) {
-    // чтобы сделать сужжение в таких случаях
-    if (a === b) {
-        // а по типу равно b ?
-        // то мы можем обращаться к a/b обращаться как к строке
-        return a.length;
-    }
-    else {
-        return b;
-    }
-}
-console.log(logMultipleIds('text', true)); // true
-console.log(logMultipleIds('text', 'text')); // true
+fetchWithAuthAll('s', 'post');
+// как мы раньше типизировали объект
+let user = {
+    name: 'asd',
+    age: 33,
+    skills: ['1', '2'],
+};
+let newUser = {
+    name: 'asd',
+    age: 33,
+    skills: ['1', '2'],
+};
+let userAll = {
+    name: 'asd',
+    age: 33,
+    skills: ['1', '2'],
+    id: 1,
+};
--- 2-Продвинутые типы/app.ts
+++ 2-Продвинутые типы/app.ts
@@ -1,56 +1,48 @@
-const arr = ['sdf', 1];
-// функция, которая будет логировать переданный нами id
-// принимает Union тип
-function logId(id: string | number | boolean) {
-  if (typeof id === 'string') {
-    console.log(id);
-  } else if (typeof id === 'number') {
-    console.log(id);
-  } else {
-    console.log(id);
-  }
+type httpMethod = 'post' | 'get';
+type coolString = string;
+
+function fetchWithAuthAll(url: string, method: httpMethod): 1 | -1 {
+  return 1;
 }
 
-// теперь можем функцию вызвать либо с number либо с string
-logId(1);
-logId('text');
-logId(true);
+fetchWithAuthAll('s', 'post');
 
-// работа со cложными типами
-// функция будет принимать либо одну ошибку либо массив ошибок
-function logError(err: string | string[]) {
-  if (Array.isArray(err)) {
-    // если это массив
-    console.log(err);
-  } else {
-    console.log(err);
-  }
-}
+// как мы раньше типизировали объект
+let user: {
+  name: string;
+  age: number;
+  skills: string[];
+} = {
+  name: 'asd',
+  age: 33,
+  skills: ['1', '2'],
+};
 
-console.log(logError(['error1', 'error2'])); // [ 'error1', 'error2' ]
+// типизацию объекта тоже можем вынести в type alies
 
-// принимаем объект, у которого есть либо свойство a/b
-function logObject(obj: { a: number } | { b: number }) {
-  // оператор in - проверяет, а есль ли тот или иной ключ в объекте
-  if ('a' in obj) {
-    console.log(obj.a);
-  } else {
-    console.log(obj.b);
-  }
-}
-logObject({ a: 10, b: 1 }); // 10
-// функция, которая принимает multiply dist
-// в обоих случаях есть тип string
-function logMultipleIds(a: string | number, b: string | boolean) {
-  // чтобы сделать сужжение в таких случаях
-  if (a === b) {
-    // а по типу равно b ?
-    // то мы можем обращаться к a/b обращаться как к строке
-    return a.length;
-  } else {
-    return b;
-  }
-}
+type User = {
+  name: string;
+  age: number;
+  skills: string[];
+};
 
-console.log(logMultipleIds('text', true)); // true
-console.log(logMultipleIds('text', 'text')); // 4
+type Role = {
+  id: number;
+};
+
+let newUser: User = {
+  name: 'asd',
+  age: 33,
+  skills: ['1', '2'],
+};
+
+// intersection целых тайпов
+// на базе двух type Role и User сделаем один intersection type
+type UserWithRole = User & Role;
+
+let userAll: UserWithRole = {
+  name: 'asd',
+  age: 33,
+  skills: ['1', '2'],
+  id: 1,
+};
