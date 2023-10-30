02ce4d3b95de35262517635daca1631b94cea4de
Vitaliy Galata <nertis44@gmail.com>
2023-10-30 23:25:44 +0200

    types/interfaces

--- 2-Продвинутые типы/app.ts
+++ 2-Продвинутые типы/app.ts
@@ -1,71 +1,23 @@
+// один интерфейс дополняет другой интерфейс
+
 interface User {
   name: string;
+}
+
+interface User {
   age: number;
-  skills: string[];
-
-  log: (id: number) => string; // функция получает id лога, и возращает инфу по логу
 }
 
-interface Role {
-  roleId: number;
-}
-
-/*
-interface UserWithRole extends User {
-  // чтобы добавить другой интрефейс,
-  // используем extends
-  roleId: number;
-}
-*/
-
-interface UserWithRole extends User, Role {
-  // через запятаю можем объединять много интерфейсов
-  createdAt: Date;
-}
-
-/*
-let user: User = {
-  // User уже будет интерфейсом
-*/
-let user: UserWithRole = {
-  name: 'asd',
-  age: 33,
-  skills: ['1', '2'],
-  roleId: 10,
-  createdAt: new Date(),
-  log(id) {
-    return `Info log: ${id}`;
-  },
+const user: User = {
+  name: 'Vitaliy',
+  age: 20,
 };
 
-// можем сделать type у которого будут все теже методы
-// здесь нет никаких различий с интерфейсом
-type User2 = {
-  name: string;
-  age: number;
-  skills: string[];
-  log: (id: number) => string;
-};
+// преимущество type
+type ID = string | number;
 
-let user2: User2 = {
-  name: 'asd',
-  age: 33,
-  skills: ['1', '2'],
-  log(id) {
-    return `Info log: ${id}`;
-  },
-};
-
-// Индексные свойства в интерфейсах
-// чаще применяются в словарях.
-// например хотим иметь некий id пользователя
-// эта запись будет работать как в интерфейсах, так и в тайпах
-
-interface UserDic {
-  [index: number]: User; // у этого интерфейса может быть не огранич. число свойств
-  // где ключем является число, а значением - пользователь
+// интерфейсы позволяют работать с объектами, описанием класа, но не с простыми типами
+// только внутри интерфейса могут лежать простые типы
+interface IDI {
+  ID: string | number;
 }
-
-type UserDic2 = {
-  [index: number]: User;
-};
