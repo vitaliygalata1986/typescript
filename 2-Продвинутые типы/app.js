3ec5130d9a8df7123c8cbcb424b51e16cb60f14a
Vitaliy Galata <nertis44@gmail.com>
2023-10-30 22:49:43 +0200

    Use interfaces

--- 2-Продвинутые типы/app.js
+++ 2-Продвинутые типы/app.js
@@ -1,57 +1,23 @@
 "use strict";
-const arr = ['sdf', 1];
-// функция, которая будет логировать переданный нами id
-// принимает Union тип
-function logId(id) {
-    if (typeof id === 'string') {
-        console.log(id);
-    }
-    else if (typeof id === 'number') {
-        console.log(id);
-    }
-    else {
-        console.log(id);
-    }
-}
-// теперь можем функцию вызвать либо с number либо с string
-logId(1);
-logId('text');
-logId(true);
-// работа со cложными типами
-// функция будет принимать либо одну ошибку либо массив ошибок
-function logError(err) {
-    if (Array.isArray(err)) {
-        // если это массив
-        console.log(err);
-    }
-    else {
-        console.log(err);
-    }
-}
-console.log(logError(['error1', 'error2'])); // [ 'error1', 'error2' ]
-// принимаем объект, у которого есть либо свойство a/b
-function logObject(obj) {
-    // оператор in - проверяет, а есль ли тот или иной ключ в объекте
-    if ('a' in obj) {
-        console.log(obj.a);
-    }
-    else {
-        console.log(obj.b);
-    }
-}
-logObject({ a: 10, b: 1 }); // 10
-// функция, которая принимает multiply dist
-// в обоих случаях есть тип string
-function logMultipleIds(a, b) {
-    // чтобы сделать сужжение в таких случаях
-    if (a === b) {
-        // а по типу равно b ?
-        // то мы можем обращаться к a/b обращаться как к строке
-        return a.length;
-    }
-    else {
-        return b;
-    }
-}
-console.log(logMultipleIds('text', true)); // true
-console.log(logMultipleIds('text', 'text')); // true
+/*
+let user: User = {
+  // User уже будет интерфейсом
+*/
+let user = {
+    name: 'asd',
+    age: 33,
+    skills: ['1', '2'],
+    roleId: 10,
+    createdAt: new Date(),
+    log(id) {
+        return `Info log: ${id}`;
+    },
+};
+let user2 = {
+    name: 'asd',
+    age: 33,
+    skills: ['1', '2'],
+    log(id) {
+        return `Info log: ${id}`;
+    },
+};
--- 2-Продвинутые типы/app.ts
+++ 2-Продвинутые типы/app.ts
@@ -1,56 +1,71 @@
-const arr = ['sdf', 1];
-// функция, которая будет логировать переданный нами id
-// принимает Union тип
-function logId(id: string | number | boolean) {
-  if (typeof id === 'string') {
-    console.log(id);
-  } else if (typeof id === 'number') {
-    console.log(id);
-  } else {
-    console.log(id);
-  }
+interface User {
+  name: string;
+  age: number;
+  skills: string[];
+
+  log: (id: number) => string; // функция получает id лога, и возращает инфу по логу
 }
 
-// теперь можем функцию вызвать либо с number либо с string
-logId(1);
-logId('text');
-logId(true);
-
-// работа со cложными типами
-// функция будет принимать либо одну ошибку либо массив ошибок
-function logError(err: string | string[]) {
-  if (Array.isArray(err)) {
-    // если это массив
-    console.log(err);
-  } else {
-    console.log(err);
-  }
+interface Role {
+  roleId: number;
 }
 
-console.log(logError(['error1', 'error2'])); // [ 'error1', 'error2' ]
-
-// принимаем объект, у которого есть либо свойство a/b
-function logObject(obj: { a: number } | { b: number }) {
-  // оператор in - проверяет, а есль ли тот или иной ключ в объекте
-  if ('a' in obj) {
-    console.log(obj.a);
-  } else {
-    console.log(obj.b);
-  }
+/*
+interface UserWithRole extends User {
+  // чтобы добавить другой интрефейс,
+  // используем extends
+  roleId: number;
 }
-logObject({ a: 10, b: 1 }); // 10
-// функция, которая принимает multiply dist
-// в обоих случаях есть тип string
-function logMultipleIds(a: string | number, b: string | boolean) {
-  // чтобы сделать сужжение в таких случаях
-  if (a === b) {
-    // а по типу равно b ?
-    // то мы можем обращаться к a/b обращаться как к строке
-    return a.length;
-  } else {
-    return b;
-  }
+*/
+
+interface UserWithRole extends User, Role {
+  // через запятаю можем объединять много интерфейсов
+  createdAt: Date;
 }
 
-console.log(logMultipleIds('text', true)); // true
-console.log(logMultipleIds('text', 'text')); // 4
+/*
+let user: User = {
+  // User уже будет интерфейсом
+*/
+let user: UserWithRole = {
+  name: 'asd',
+  age: 33,
+  skills: ['1', '2'],
+  roleId: 10,
+  createdAt: new Date(),
+  log(id) {
+    return `Info log: ${id}`;
+  },
+};
+
+// можем сделать type у которого будут все теже методы
+// здесь нет никаких различий с интерфейсом
+type User2 = {
+  name: string;
+  age: number;
+  skills: string[];
+  log: (id: number) => string;
+};
+
+let user2: User2 = {
+  name: 'asd',
+  age: 33,
+  skills: ['1', '2'],
+  log(id) {
+    return `Info log: ${id}`;
+  },
+};
+
+// Индексные свойства в интерфейсах
+// чаще применяются в словарях.
+// например хотим иметь некий id пользователя
+// эта запись будет работать как в интерфейсах, так и в тайпах
+
+interface UserDic {
+  [index: number]: User; // у этого интерфейса может быть не огранич. число свойств
+  // где ключем является число, а значением - пользователь
+}
+
+type UserDic2 = {
+  [index: number]: User;
+};
