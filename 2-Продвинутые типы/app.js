2d3d2f631ad881e5d114ac2bcbc9560afc435a6d
vitaliy <nertis44@gmail.com>
2023-11-05 22:45:14 +0200

    use optional types

--- 2-Продвинутые типы/app.js
+++ 2-Продвинутые типы/app.js
@@ -1,23 +1,34 @@
-"use strict";
-/*
-let user: User = {
-  // User уже будет интерфейсом
-*/
-let user = {
-    name: 'asd',
-    age: 33,
-    skills: ['1', '2'],
-    roleId: 10,
-    createdAt: new Date(),
-    log(id) {
-        return `Info log: ${id}`;
-    },
+// пароль наш будет опцинальным
+var user = {
+    login: 'admin',
 };
-let user2 = {
-    name: 'asd',
-    age: 33,
-    skills: ['1', '2'],
-    log(id) {
-        return `Info log: ${id}`;
-    },
+// тоже самое можем обзначить, что второе свойство является не обязательным
+function multiply(first, second) {
+    if (!second) {
+        return first * first;
+    }
+    return first * second;
+}
+console.log(multiply(5)); //25
+console.log(multiply(5, 3)); //15
+function testPass(user) {
+    var _a;
+    var typePassword = (_a = user.password) === null || _a === void 0 ? void 0 : _a.type; // ts добавил и сюда ?
+    // это называется optional chaining
+    // это обозначает, что мы можем обратится к свойству объекта, которого может не быть
+    // ! - обозначает, что такое свойство точно есть
+    // const typePasswordNew = user.password!.type;
+    // или вариант сужжения типов
+    // const newtypePassword = user.password ? user.password.type : undefined;
+    return typePassword;
+}
+var User = {
+    login: 'admin',
 };
+console.log(testPass(User)); // undefined
+function test(param) {
+    var t = param !== null && param !== void 0 ? param : multiply(5); // если param null/undefined, то отработает multiply()
+    return t;
+}
+console.log(test()); // 25
+console.log(test('stroka')); // stroka
--- 2-Продвинутые типы/app.ts
+++ 2-Продвинутые типы/app.ts
@@ -1,23 +1,55 @@
-// один интерфейс дополняет другой интерфейс
-
 interface User {
-  name: string;
+  login: string;
+  password?: string; // ? - обозначает опциональность этого свойства
 }
 
-interface User {
-  age: number;
-}
+// пароль наш будет опцинальным
 
 const user: User = {
-  name: 'Vitaliy',
-  age: 20,
+  login: 'admin',
 };
 
-// преимущество type
-type ID = string | number;
+// тоже самое можем обзначить, что второе свойство является не обязательным
+function multiply(first: number, second?: number): number {
+  if (!second) {
+    return first * first;
+  }
 
-// интерфейсы позволяют работать с объектами, описанием класа, но не с простыми типами
-// только внутри интерфейса могут лежать простые типы
-interface IDI {
-  ID: string | number;
+  return first * second;
 }
+console.log(multiply(5)); //25
+console.log(multiply(5, 3)); //15
+
+interface UserPro {
+  login: string;
+  password?: {
+    type: 'primary' | 'secondary';
+  };
+}
+
+function testPass(user: UserPro) {
+  const typePassword = user.password?.type; // ts добавил и сюда ?
+  // это называется optional chaining
+  // это обозначает, что мы можем обратится к свойству объекта, которого может не быть
+
+  // ! - обозначает, что такое свойство точно есть
+  // const typePasswordNew = user.password!.type;
+
+  // или вариант сужжения типов
+  // const newtypePassword = user.password ? user.password.type : undefined;
+
+  return typePassword;
+}
+
+let User = {
+  login: 'admin',
+};
+console.log(testPass(User)); // undefined
+
+function test(param?: string) {
+  const t = param ?? multiply(5); // если param null/undefined, то отработает multiply()
+  return t;
+}
+
+console.log(test()); // 25
+console.log(test('stroka')); // stroka
