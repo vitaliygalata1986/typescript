9e1ed043c610ed3696e7231c1f0f894ebd4840c0
vitaliy <nertis44@gmail.com>
2023-11-21 11:27:52 +0200

    use never

--- 2-Продвинутые типы/app.ts
+++ 2-Продвинутые типы/app.ts
@@ -1,56 +1,47 @@
-const arr = ['sdf', 1];
-// функция, которая будет логировать переданный нами id
-// принимает Union тип
-function logId(id: string | number | boolean) {
-  if (typeof id === 'string') {
-    console.log(id);
-  } else if (typeof id === 'number') {
-    console.log(id);
-  } else {
-    console.log(id);
+// функция генерит наш ошибку
+function genereateError(message: string): never {
+  // never - функция никогда не вернеться
+  throw new Error(message);
+}
+
+function dumpError(): never {
+  // return ''
+  while (true) {}
+}
+
+function rec(): never {
+  return rec();
+}
+
+// const a: never = null; // мы не можем never ничего присвоить
+
+const a: void = undefined; // void можем присвоить undefined
+// так как, когда мы возращаем  функцию, то фактически возвращается undefined
+
+// реальное применение с пользой
+type paymentAction = 'refund' | 'checkout' | 'reject';
+
+function processAction(action: paymentAction) {
+  switch (action) {
+    case 'refund':
+      // ....
+      break;
+    case 'checkout':
+      // ....
+      break;
+    default:
+      // чтобы TypeScript не ругался на не использованную переменную
+      // можно применять _
+      const _: never = action; // здесь action сам never, и сюда проверка никогда не попадет
+      throw new Error('Нет такого action');
   }
 }
 
-// теперь можем функцию вызвать либо с number либо с string
-logId(1);
-logId('text');
-logId(true);
-
-// работа со cложными типами
-// функция будет принимать либо одну ошибку либо массив ошибок
-function logError(err: string | string[]) {
-  if (Array.isArray(err)) {
-    // если это массив
-    console.log(err);
-  } else {
-    console.log(err);
+function isString(x: string | number): boolean {
+  if (typeof x == 'string') {
+    return true;
+  } else if (typeof x == 'number') {
+    return false;
   }
+  genereateError('какой-то текст ошибки');
 }
-
-console.log(logError(['error1', 'error2'])); // [ 'error1', 'error2' ]
-
-// принимаем объект, у которого есть либо свойство a/b
-function logObject(obj: { a: number } | { b: number }) {
-  // оператор in - проверяет, а есль ли тот или иной ключ в объекте
-  if ('a' in obj) {
-    console.log(obj.a);
-  } else {
-    console.log(obj.b);
-  }
-}
-logObject({ a: 10, b: 1 }); // 10
-// функция, которая принимает multiply dist
-// в обоих случаях есть тип string
-function logMultipleIds(a: string | number, b: string | boolean) {
-  // чтобы сделать сужжение в таких случаях
-  if (a === b) {
-    // а по типу равно b ?
-    // то мы можем обращаться к a/b обращаться как к строке
-    return a.length;
-  } else {
-    return b;
-  }
-}
-
-console.log(logMultipleIds('text', true)); // true
-console.log(logMultipleIds('text', 'text')); // 4
